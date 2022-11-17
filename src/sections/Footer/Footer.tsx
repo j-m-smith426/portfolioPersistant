007697514504565b0d850ec91d3480d59a992956
j-m-smith426 <joabsmith4226@gmail.com>
2022-11-17 14:26:54 -0500

    footer

--- src/sections/Footer/Footer.tsx
+++ src/sections/Footer/Footer.tsx
@@ -1,10 +1,20 @@
 import { FC } from "react";
+import Link from "../../components/Link/Link";
 import "./Footer.scss";
 
 interface IFooterProps {}
 
 const Footer: FC<IFooterProps> = (props) => {
-	return <footer className="footer">Footer</footer>;
+	return (
+		<footer className="footer">
+			<p>
+				<div className="top-nav__icon">JS</div> Joab Smith
+			</p>
+			<Link href="/" class={"--nav footer--btn"}>
+				Back to Top
+			</Link>
+		</footer>
+	);
 };
 
 export default Footer;
