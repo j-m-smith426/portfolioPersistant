593f242f7fe416f853bcff945b0233e8e968d305
j-m-smith426 <joabsmith4226@gmail.com>
2022-11-17 21:35:01 -0500

    fixed build errors

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 import "./Header.scss";
-import profile from "../../assets/JoabSmith.jpg";
 import Link from "../../components/Link/Link";
 
 interface IHeaderProps {}
@@ -10,7 +9,11 @@ const Header: FC<IHeaderProps> = (props) => {
 		<header className="header">
 			<h2 className="heading-2">Joab Smith</h2>
 			<h3 className="heading-3">Software Engineer</h3>
-			<img src={profile} alt="Profile Picture" className="header__img" />
+			<img
+				src="/assets/JoabSmith.jpg"
+				alt="Profile Picture"
+				className="header__img"
+			/>
 
 			<Link href="/assets/JoabSmithResume.pdf" class="--main" resume>
 				View Resume
